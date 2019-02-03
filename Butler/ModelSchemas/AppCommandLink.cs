6ffef2ee3267bec9a1686a19a6459b310531fc90
Andrew Piper <andrew@threax.com>
2019-02-03 11:52:32 -0500

    Added models.

---
+++ Butler/ModelSchemas/AppCommandLink.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Threax.AspNetCore.Models;
+
+namespace Butler.ModelSchemas
+{
+    public class AppCommandLink
+    {
+        [NoInputModel]
+        [NoViewModel]
+        public String Json { get; set; }
+
+        public AppCommandSet CommandSet { get; set; }
+    }
+}
---
+++ Butler/ModelSchemas/AppCommandSet.cs
@@ -0,0 +1,30 @@
+﻿using Butler.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Threax.AspNetCore.Models;
+
+namespace Butler.ModelSchemas
+{
+    public abstract class AppCommandSet
+    {
+        [Required]
+        [MaxLength(450)]
+        public String Name { get; set; }
+
+        [MaxLength(2000)]
+        public String VoicePrompt { get; set; }
+
+        [MaxLength(2000)]
+        public String Response { get; set; }
+
+        [MaxLength(10)]
+        public String Key { get; set; }
+
+        public KeyModifier MyProperty { get; set; }
+
+        public List<AppCommandLink> Links { get; set; }
+    }
+}
