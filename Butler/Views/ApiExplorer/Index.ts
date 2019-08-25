7b97e765f072fd3b6bea2327aa46921914c8d7cd
Andrew Piper <andrew@threax.com>
2019-08-25 04:17:35 -0400

    Removed butler.

--- Butler/Views/ApiExplorer/Index.ts
+++
@@ -1,9 +0,0 @@
-﻿import * as explorer from 'hr.halcyon-explorer.HalcyonBrowserController';
-import * as controller from 'hr.controller';
-import * as startup from 'clientlibs.startup';
-import * as deepLink from 'hr.deeplink';
-
-var builder = startup.createBuilder();
-deepLink.addServices(builder.Services);
-explorer.addServices(builder.Services);
-explorer.createBrowser(builder, new explorer.BrowserOptions);
