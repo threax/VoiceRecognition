e9f913a09eb34087a4ed09e3277a0ea768015802
Andrew Piper <andrew@threax.com>
2019-11-23 08:26:26 -0500

    Fixed dev path, but shell still wont load.

--- Publish.ps1
+++ Publish.ps1
@@ -9,7 +9,7 @@ param (
 $currentDir = "$pwd"
 
 $installPath = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community"
-Import-Module (Join-Path $installPath "Common7\Tools\vsdevshell\Microsoft.VisualStudio.DevShell.dll")
+Import-Module (Join-Path $installPath "Common7\Tools\Microsoft.VisualStudio.DevShell.dll")
 Enter-VsDevShell -VsInstallPath $installPath
 
 Set-Location $currentDir
